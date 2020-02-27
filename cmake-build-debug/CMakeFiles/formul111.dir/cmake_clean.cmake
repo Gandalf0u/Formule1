ad0b5b25cd27a6d8cc73eebe08492b1b27350df1
Sharkerz <292691@supinfo.com>
2020-02-27 11:29:12 +0100

    add release

---
+++ cmake-build-debug/CMakeFiles/formul111.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formul111.dir/Piste.cpp.obj"
+  "CMakeFiles/formul111.dir/Voiture.cpp.obj"
+  "CMakeFiles/formul111.dir/main.cpp.obj"
+  "formul111.exe"
+  "formul111.exe.manifest"
+  "formul111.pdb"
+  "libformul111.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formul111.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
